0a3df11f0ae299b09be67db982fe76d763fc6e0a
alexiscartechini <alexiscartechini@gmail.com>
2026-01-17 11:26:42 +0100

    Alexis Cartechini - project setup

---
+++ src/main/java/com/volkswagen/preferencecenter/PreferenceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.volkswagen.preferencecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferenceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreferenceCenterApplication.class, args);
+	}
+
+}
