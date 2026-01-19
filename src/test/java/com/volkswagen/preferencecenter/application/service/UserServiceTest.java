0f9872da49fc04b8f7dab450d56e9a154e751f78
alexiscartechini <alexiscartechini@gmail.com>
2026-01-19 18:08:28 +0100

    Alexis Cartechini - taking off chooseNotification from UserServiceTest

--- src/test/java/com/volkswagen/preferencecenter/application/service/UserServiceTest.java
+++ src/test/java/com/volkswagen/preferencecenter/application/service/UserServiceTest.java
@@ -1,10 +1,6 @@
 package com.volkswagen.preferencecenter.application.service;
 
-import com.volkswagen.preferencecenter.domain.model.Consent;
-import com.volkswagen.preferencecenter.domain.model.ConsentEvent;
-import com.volkswagen.preferencecenter.domain.model.ConsentType;
 import com.volkswagen.preferencecenter.domain.model.User;
-import com.volkswagen.preferencecenter.domain.port.ConsentPersistencePort;
 import com.volkswagen.preferencecenter.domain.port.UserPersistencePort;
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +14,7 @@ class UserServiceTest {
     private static final String NOT_UNIQUE_EMAIL = "not_unique_email@email.com";
     private static final String UNIQUE_EMAIL = "unique_email@email.com";
     private final UserPersistencePort userPersistencePort = mock(UserPersistencePort.class);
-    private final ConsentPersistencePort consentPersistencePort = mock(ConsentPersistencePort.class);
-    private final UserService userService = new UserService(userPersistencePort, consentPersistencePort);
+    private final UserService userService = new UserService(userPersistencePort);
 
     @Test
     void shouldReturnTrueIfEmailIsUnique(){
@@ -31,11 +26,5 @@ class UserServiceTest {
     void shouldReturnFalseIfEmailIsNotUnique(){
         when(userPersistencePort.findByEmail(NOT_UNIQUE_EMAIL)).thenReturn(Optional.of(new User(NOT_UNIQUE_EMAIL)));
         assertFalse(userService.isUniqueEmail(NOT_UNIQUE_EMAIL));
-    }
-
-    @Test
-    void shouldCreateAConsentEventWhenUserChooseANotificationMeans(){
-        userService.chooseNotification(new Consent("id", ConsentType.EMAIL_NOTIFICATIONS, true));
-        verify(consentPersistencePort).save(any(ConsentEvent.class));
     }
 }
