49b33144bc0b383221f77be5f2aaf5482346b4f0
alexiscartechini <alexiscartechini@gmail.com>
2026-01-19 09:36:45 +0100

    Alexis Cartechini - adding chooseNotification method

--- src/main/java/com/volkswagen/preferencecenter/application/service/UserService.java
+++ src/main/java/com/volkswagen/preferencecenter/application/service/UserService.java
@@ -1,18 +1,29 @@
 package com.volkswagen.preferencecenter.application.service;
 
+import com.volkswagen.preferencecenter.domain.model.Consent;
+import com.volkswagen.preferencecenter.domain.model.ConsentEvent;
+import com.volkswagen.preferencecenter.domain.port.ConsentPersistencePort;
 import com.volkswagen.preferencecenter.domain.port.UserPersistencePort;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class UserService {
 
     private final UserPersistencePort userPersistencePort;
+    private final ConsentPersistencePort consentPersistencePort;
 
-    public UserService(UserPersistencePort userPersistencePort) {
+    public UserService(UserPersistencePort userPersistencePort, ConsentPersistencePort consentPersistencePort) {
         this.userPersistencePort = userPersistencePort;
+        this.consentPersistencePort = consentPersistencePort;
     }
 
     public boolean isUniqueEmail(String email) {
         return userPersistencePort.findByEmail(email).isEmpty();
+    }
+
+    public void chooseNotification(Consent consent) {
+        consentPersistencePort.save(new ConsentEvent(consent, Instant.now()));
     }
 }
--- src/test/java/com/volkswagen/preferencecenter/application/service/UserServiceTest.java
+++ src/test/java/com/volkswagen/preferencecenter/application/service/UserServiceTest.java
@@ -1,22 +1,40 @@
 package com.volkswagen.preferencecenter.application.service;
 
+import com.volkswagen.preferencecenter.domain.model.Consent;
+import com.volkswagen.preferencecenter.domain.model.ConsentEvent;
+import com.volkswagen.preferencecenter.domain.model.ConsentType;
+import com.volkswagen.preferencecenter.domain.model.User;
+import com.volkswagen.preferencecenter.domain.port.ConsentPersistencePort;
 import com.volkswagen.preferencecenter.domain.port.UserPersistencePort;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
+    private static final String NOT_UNIQUE_EMAIL = "not_unique_email@email.com";
     private final UserPersistencePort userPersistencePort = mock(UserPersistencePort.class);
-    private final UserService userService = new UserService(userPersistencePort);
+    private final ConsentPersistencePort consentPersistencePort = mock(ConsentPersistencePort.class);
+    private final UserService userService = new UserService(userPersistencePort, consentPersistencePort);
 
     @Test
     void shouldReturnTrueIfEmailIsUnique(){
         when(userPersistencePort.findByEmail("unique_email@email.com")).thenReturn(Optional.empty());
         assertTrue(userService.isUniqueEmail("unique_email@email.com"));
+    }
+
+    @Test
+    void shouldReturnFalseIfEmailIsNotUnique(){
+        when(userPersistencePort.findByEmail(NOT_UNIQUE_EMAIL)).thenReturn(Optional.of(new User()));
+        assertFalse(userService.isUniqueEmail(NOT_UNIQUE_EMAIL));
+    }
+
+    @Test
+    void shouldCreateAConsentEventWhenUserChooseANotificationMeans(){
+        userService.chooseNotification(new Consent("id", ConsentType.EMAIL_NOTIFICATIONS, true));
+        verify(consentPersistencePort).save(any(ConsentEvent.class));
     }
 }
