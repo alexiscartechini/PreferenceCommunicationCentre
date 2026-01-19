ddd5eb571a6199746fa06b27223be2141ab15567
alexiscartechini <alexiscartechini@gmail.com>
2026-01-19 18:10:20 +0100

    Alexis Cartechini - Adding ConsentController

---
+++ src/main/java/com/volkswagen/preferencecenter/infrastructure/controller/ConsentController.java
@@ -0,0 +1,24 @@
+package com.volkswagen.preferencecenter.infrastructure.controller;
+
+import com.volkswagen.preferencecenter.dto.UpdateConsentsRequest;
+import com.volkswagen.preferencecenter.application.service.ConsentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/consents")
+public class ConsentController {
+
+    ConsentService consentService;
+
+    public ConsentController(ConsentService consentService){
+        this.consentService = consentService;
+    }
+
+    @PostMapping
+    public void chooseConsent(@RequestBody UpdateConsentsRequest updateConsentsRequest){
+        consentService.chooseConsent(updateConsentsRequest);
+    }
+}
