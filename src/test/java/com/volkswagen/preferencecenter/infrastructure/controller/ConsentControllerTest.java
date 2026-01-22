3daf6f2e48b5d083504f38495adcca01731d1a05
alexiscartechini <alexiscartechini@gmail.com>
2026-01-22 09:50:37 +0100

    Alexis Cartechini - adding coverage to ConsentController

---
+++ src/test/java/com/volkswagen/preferencecenter/infrastructure/controller/ConsentControllerTest.java
@@ -0,0 +1,29 @@
+package com.volkswagen.preferencecenter.infrastructure.controller;
+
+import com.volkswagen.preferencecenter.application.service.ConsentService;
+import com.volkswagen.preferencecenter.dto.UpdateConsentsRequest;
+import com.volkswagen.preferencecenter.dto.UserReference;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class ConsentControllerTest {
+
+    ConsentService consentService = mock(ConsentService.class);
+    ConsentController consentController = new ConsentController(consentService);
+
+    @Test
+    void shouldChangeConsent() {
+        UUID userId = UUID.randomUUID();
+        UpdateConsentsRequest updateConsentsRequest =
+                new UpdateConsentsRequest(new UserReference(userId), List.of());
+
+        consentController.changeConsent(updateConsentsRequest);
+
+        verify(consentService).changeConsent(updateConsentsRequest);
+    }
+}
